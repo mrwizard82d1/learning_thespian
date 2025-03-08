acb8cafb0fdc0fd2c18dfd5b3211ff34361aa339
Larry Jones <mrwizard82d1@gmail.com>
2025-03-08 12:50:03 -0600

    "Hello, World!" using thespian

--- quick-start/hello_world.py
+++ quick-start/hello_world.py
@@ -1,5 +1,21 @@
+from thespian.actors import (
+    Actor,
+    ActorExitRequest,
+    ActorSystem,
+)
+
+
+class Hello(Actor):
+    def receiveMessage(self, msg, sender):
+        # NOTE: The Hello Actor sends the same message to `sender`
+        # in response to **all** messages.
+        self.send(sender, 'Hello, Thespian World!')
+
+
 def main():
-    print("Hello from quick-start!")
+    hello = ActorSystem().createActor(Hello)
+    print(ActorSystem().ask(hello, 'hi', 1))
+    ActorSystem().tell(hello, ActorExitRequest())
 
 
 if __name__ == "__main__":
