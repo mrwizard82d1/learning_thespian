319b1d95125fad8fe59ebb18fa10a8c032827d93
Larry Jones <mrwizard82d1@gmail.com>
2025-03-08 18:17:19 -0600

    Single instance of `World` actor

--- thespian-features/hellogoodbye.py
+++ thespian-features/hellogoodbye.py
@@ -7,9 +7,10 @@ from thespian.actors import ActorSystem
 class Hello(thea.Actor):
     def receiveMessage(self, message, sender):
         if message == 'are you there?':
-            world = self.createActor(World)
+            if not hasattr(self, 'world'):
+                self.world = self.createActor(World)
             world_msg = (sender, 'Hello,')
-            self.send(world, world_msg)
+            self.send(self.world, world_msg)
 
 
 class World(thea.Actor):
@@ -25,8 +26,13 @@ class Goodbye(thea.Actor):
 
 
 def run_example(systembase=None):
-    hello = thea.ActorSystem(systembase).createActor(Hello)
     goodbye = thea.ActorSystem().createActor(Goodbye)
+    hello = thea.ActorSystem(systembase).createActor(Hello)
+    # First message to an instance
+    greeting = ActorSystem().ask(hello, 'are you there?', 1.5)
+    print(greeting + '\n' + ActorSystem().ask(goodbye, None, 0.1))
+
+    # Second message to the same insntance
     greeting = ActorSystem().ask(hello, 'are you there?', 1.5)
     print(greeting + '\n' + ActorSystem().ask(goodbye, None, 0.1))
     ActorSystem().shutdown()
